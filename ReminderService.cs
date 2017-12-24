a29906cd1358e47a534912dadc8add65cfa4adb4
fallen888 <k@mayarovich.com>
2017-12-23 19:11:26 -0500

    Fixed array initialization bug.

--- ReminderService.cs
+++ ReminderService.cs
@@ -304,7 +304,7 @@ namespace MyTesla
 
 
         protected void CleanupOldErrors() {
-            var errorKeys = new string[0];
+            var errorKeys = new string[trackedErrors.Keys.Count];
             trackedErrors.Keys.CopyTo(errorKeys, 0);
 
             foreach (var key in errorKeys) {
